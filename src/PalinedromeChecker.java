0f4e7aee14d2f353a67ed0bf7d95e3c6a077770e
Harishkrai7 <harishkumarrai517@gmail.com>
2026-02-27 15:07:30 +0530

    UC2:Hardcoded Palinedrome

--- src/PalinedromeChecker.java
+++ src/PalinedromeChecker.java
@@ -17,3 +17,25 @@ public class PalinedromeChecker {
         System.out.println("---------------------------------------");
     }
 }
+
+public class PalinedromeChecker {
+
+    public static void main(String[] args) {
+
+        String input = "madam";
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome.");
+        } else {
+            System.out.println(input + " is NOT a Palindrome.");
+        }
+    }
+}
