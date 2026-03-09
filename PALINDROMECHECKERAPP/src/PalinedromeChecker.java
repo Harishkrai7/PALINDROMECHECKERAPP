88a902f84fce38036fac414b3f29808432d6daa3
Harishkrai7 <harishkumarrai517@gmail.com>
2026-03-09 15:45:11 +0530

    UC9: Recursive palindrome checker using recursion and call stack

--- PALINDROMECHECKERAPP/src/PalinedromeChecker.java
+++ PALINDROMECHECKERAPP/src/PalinedromeChecker.java
@@ -215,3 +215,30 @@ public static void main(String[] args) {
     else
         System.out.println("Not Palindrome");
 }
+
+// Recursive palindrome check
+public static boolean isPalindrome(String str, int start, int end) {
+
+    if(start >= end)
+        return true;
+
+    if(str.charAt(start) != str.charAt(end))
+        return false;
+
+    return isPalindrome(str, start + 1, end - 1);
+}
+
+public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter string: ");
+    String s = sc.nextLine();
+
+    s = s.replaceAll("\\s+","").toLowerCase();
+
+    if(isPalindrome(s, 0, s.length() - 1))
+        System.out.println("Palindrome");
+    else
+        System.out.println("Not Palindrome");
+}
